8014cf710dcd0d1544221457f50cc9bd85aa4e11
vinodhkumargaggera <48786171+vinodhkumargaggera@users.noreply.github.com>
2019-05-17 20:48:15 +0530

    first commit

---
+++ blogproject/blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from blog.models import Post, Comment
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title','slug','author','body','publish','created','updated','status']
+    prepopulated_fields={'slug':('title',)}
+    list_filter=('status','author','created','publish')
+    search_fields=('title','body')
+    raw_id_fields=('author',)
+    date_hierarchy='publish'
+    ordering=['status','publish']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['name','email','body','created','updated','active']
+    list_filter=('active','created','updated')
+    search_fields=('name','body','email')
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
